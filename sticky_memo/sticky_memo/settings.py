1d8d0e8f2298df6a19d1d97914b4d1ac33d2417e
WonBeomChoi <31643985+WonBeomChoi@users.noreply.github.com>
2023-06-13 11:36:12 +0000

    settings.py 수정하기

--- sticky_memo/sticky_memo/settings.py
+++ sticky_memo/sticky_memo/settings.py
@@ -129,3 +129,4 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CSRF_TRUSTED_ORIGINS = ["https://wonbeomchoi-expert-guacamole-6vjqr656pjqfrqxq-8000.preview.app.github.dev"]
