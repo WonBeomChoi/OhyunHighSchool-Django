426c40ac2066220ecf05cc264610493ffdfd2f57
WonBeomChoi <31643985+WonBeomChoi@users.noreply.github.com>
2023-06-13 11:39:06 +0000

    template사용하기

--- sticky_memo/memo/views.py
+++ sticky_memo/memo/views.py
@@ -4,10 +4,6 @@ from memo.models import Memo
 # Create your views here.
 def get_all_memo(request):
     memo_list = Memo.objects.all()
-    html = """
-    <h1>메모들!</h1>
-    """
-    for memo in memo_list:
-        html += f"<h2>{memo.title}</h2>"
-        html += f"<p>{memo.content}</p>"
-    return HttpResponse(html)
+    context = {"memos": memo_list}
+
+    return render(request, "memo_list.html", context)
