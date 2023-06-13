e4d166cfb49362f9292f83003068cb6a79cf25d1
WonBeomChoi <31643985+WonBeomChoi@users.noreply.github.com>
2023-06-13 11:17:26 +0000

    add view

--- sticky_memo/memo/views.py
+++ sticky_memo/memo/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from memo.models import Memo
 
 # Create your views here.
+def get_all_memo(request):
+    memo_list = Memo.objects.all()
+    return HttpResponse("<h1> 메모들 </h1>"+ str(memo_list))
